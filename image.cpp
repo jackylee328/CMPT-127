a60960598acc6dc4de8acae1b048b47eab94bf4d
Jacky Lee <jackylee328@gmail.com>
2017-09-24 16:07:22 -0700

    Add files via upload

---
+++ image.cpp
@@ -0,0 +1,24 @@
+#include <stdint.h> 
+#include "image.hpp"
+
+
+Image::Image(){
+	return EXIT_SUCCESS;
+}
+  
+
+Image::~Image(){
+	return EXIT_SUCCESS;
+}
+ 
+int Image::resize( unsigned int width,  unsigned int height, uint8_t fillcolor ){
+    return EXIT_SUCCESS;
+}
+
+int Image::set_pixel( unsigned int x, unsigned int y, uint8_t color ){
+    return EXIT_SUCCESS;
+}
+  
+int Image::get_pixel( unsigned int x, unsigned int y, uint8_t* colorp ){
+  	return EXIT_SUCCESS;
+}
